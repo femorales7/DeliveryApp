ddf838e9a5ab008fd9d8e9d23fed29d92c32300b
Your Name <femorales7@gmail.com>
2023-09-14 13:50:01 -0600

    my first comment with homeScrren already created

---
+++ screens/HomeScreen.js
@@ -0,0 +1,59 @@
+import { View, Text, SafeAreaView, Image, TextInput } from "react-native";
+import React, { useLayoutEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
+import {
+  UserIcon,
+  ChevronDownIcon,
+  SearchIcon,
+  AdjustmentsIcon,
+  AdjustmentsVerticalIcon,
+  MagnifyingGlassIcon,
+} from "react-native-heroicons/outline";
+
+const HomeScreen = () => {
+  const navigation = useNavigation();
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+  return (
+    <SafeAreaView className="bg-white  pt-8">
+      <Text>
+        {/* header */}
+        <View className="flex flex-row pb-3 items-center mx-4 space-x-2 px-4">
+          <Image
+            source={{
+              uri: "https://links.papareact.com/wru",
+            }}
+            className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+          />
+          <View className="flex-1">
+            <Text className="font-bold text-gray-400 text-xs">
+              Deliver now!
+            </Text>
+            <Text className="font-bold text-xl">
+              Current Location
+              <ChevronDownIcon size={20} color="#00CCBB" />
+            </Text>
+          </View>
+          <UserIcon className="space-x-4" size={35} color="#00CCBB" />
+        </View>
+
+        {/* Search */}
+        <View className="flex-row items-center space-x-2 pb-2 mx-4 px-4">
+          <View className="flex-row space-x-2 flex-1 bg-gray-200 p-3 ">
+            <MagnifyingGlassIcon size={20} color="gray" />
+            <TextInput
+              placeholder="Resturants and cusines"
+              keyboardType="default"
+            />
+          </View>
+          <AdjustmentsVerticalIcon className="space-x-4"  color="#00CCBB" />
+        </View>
+      </Text>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
